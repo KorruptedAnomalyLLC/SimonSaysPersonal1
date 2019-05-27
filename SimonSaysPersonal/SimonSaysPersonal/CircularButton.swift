bb964163b13889f470bd8de6a4c2bc239cbd2647
Austin West <korruptedanomaly@gmail.com>
2019-05-27 15:45:17 -0600

    KorruptioN Says with eyeballs

--- SimonSaysPersonal/SimonSaysPersonal/CircularButton.swift
+++ SimonSaysPersonal/SimonSaysPersonal/CircularButton.swift
@@ -10,17 +10,16 @@ import UIKit
 
 class CircularButton: UIButton {
 
-    override func awakeFromNib() {
+   /* override func awakeFromNib() {
         layer.cornerRadius = frame.size.width/2
         layer.masksToBounds = true
-    }
-
+    }   */
     override var isHighlighted: Bool {
         didSet {
             if isHighlighted {
                 alpha = 1.0
             } else {
-                alpha = 0.5
+                alpha = 0.3
             }
         }
     }
